18eafa1e50a8dfddace13d440eaee205d9dd6d84
Icenowy Zheng <uwu@icenowy.me>
2025-01-16 09:05:31 +0800

    zink: Do not use demote on IMG blobs
    The implementation of demote in IMG blobs seems to be a piece of hack,
    and the current use of it in Zink leads to assertion failure with
    information "Uniflex does not support demote and terminate in the same
    shader".
    
    Disable usage of demote for IMG blobs.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33051>

--- src/gallium/drivers/zink/zink_types.h
+++ src/gallium/drivers/zink/zink_types.h
@@ -1534,6 +1534,7 @@ struct zink_screen {
       bool needs_zs_shader_swizzle;
       bool needs_sanitised_layer;
       bool io_opt;
+      bool broken_demote;
    } driver_compiler_workarounds;
    struct {
       bool broken_l4a4;
